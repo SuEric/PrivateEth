6f4bedec84f2511fad093a54fe61bd48ba699e93
SuEric <suericgp@gmail.com>
2018-03-07 17:44:59 -0600

    chore(env): add basic ethereum configs in env file

---
+++ config/env/index.js
@@ -0,0 +1,7 @@
+export default {
+  ethereum: {
+    provider: 'https://rinkeby.infura.io/',
+    address: '0x7d8E32622C12feff09e5D4d969a004260Ff3d215',
+    key: '6d84923f844650637e2ddd209284e788f4755c899043d1aad4a19d02cf85ff19',
+  },
+};
